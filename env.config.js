d69984a0219628511f4ea6024961f16ec8a64c44
Jack Chan <723907+fulicat@users.noreply.github.com>
2019-05-31 13:37:48 +0800

    update
    update

--- env.config.js
+++ env.config.js
@@ -3,29 +3,29 @@
 * @Author: Jack.Chan (971546@qq.com)
 * @Date:   2019-05-31 11:41:33
 * @Last Modified by:   Jack.Chan
-* @Last Modified time: 2019-05-31 11:47:30
-* @version v1.0
+* @Last Modified time: 2019-05-31 13:36:13
+* @version v1.0.1
 */
 
 module.exports = {
 	development: {
 		publicPath: '/',
 		variables: {
-			api_baseURL: '//daily.shebaotong.com/partners',
+			api_baseURL: '//daily.fulicat.com/api',
 			api_timeout: 5000
 		}
 	},
 	daily: {
-		publicPath: '//cdn.hrwork.com/fulicat/sbt_partners_dashboard/daily/dist/',
+		publicPath: '//cdn.fulicat.com/fulicat/dashboard/daily/dist/',
 		variables: {
-			api_baseURL: '//daily.shebaotong.com/partners',
+			api_baseURL: '//daily.fulicat.com/api',
 			api_timeout: 30000
 		}
 	},
 	production: {
-		publicPath: '//cdn.hrwork.com/fulicat/sbt_partners_dashboard/master/dist/',
+		publicPath: '//cdn.fulicat.com/fulicat/dashboard/master/dist/',
 		variables: {
-			api_baseURL: '//www.shebaotong.com/partners',
+			api_baseURL: '//www.fulicat.com/api',
 			api_timeout: 30000
 		}
 	}
